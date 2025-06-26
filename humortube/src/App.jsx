281f0146ab76cefc0bd5da0aedb0b1c4d0c6b172
kimdoyunsiriuseco <dreamparkdy@gmail.com>
2025-06-26 11:17:03 +0900

    유머튜브에 올림

---
+++ humortube/src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  const apple=1
+ 
+  return (
+  <div>
+    <h1>내가좋아하는 유튜브      
+    </h1>
+    <h2>내가 좋아하는 유투브 영상만 정리</h2>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/yI5CAY6SSoM?si=Lje7kQYQQiXA9FjJ&amp;start=680" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+<h2>내가 좋아하는 유투브 노래영상</h2>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/0nqBjp-M0_8?si=TvhEskGpCJD43Db1&amp;start=427" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+
+    </div>
+  )
+}
+
+export default App
