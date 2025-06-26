3f4b0dc979b6f1b1bcd65f8587aac52d2ec0078f
kimdoyunsiriuseco <dreamparkdy@gmail.com>
2025-06-26 14:13:34 +0900

    gps

--- humortube/src/App.jsx
+++ humortube/src/App.jsx
@@ -2,11 +2,14 @@ function App() {
   const apple=1
  
   return (
-  <div>
+  <div className="center_div">
     <h1>내가좋아하는 유튜브      
     </h1>
     <h2>내가 좋아하는 유투브 영상만 정리</h2>
 <iframe width="560" height="315" src="https://www.youtube.com/embed/yI5CAY6SSoM?si=Lje7kQYQQiXA9FjJ&amp;start=680" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/e-aU4-p23BA?si=AYRWcilAcawK2bFg&amp;start=184" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
 <h2>내가 좋아하는 유투브 노래영상</h2>
 <iframe width="560" height="315" src="https://www.youtube.com/embed/0nqBjp-M0_8?si=TvhEskGpCJD43Db1&amp;start=427" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 
